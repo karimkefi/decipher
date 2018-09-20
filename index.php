779f4acf4d4cb605d23df8db7ee43fc156a960ba
Karim Kefi <karim.kefi@mayden.co.uk>
2018-09-20 17:05:24 +0100

    Cleaned up order of code and started on function explanations

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <?php
 
+
+//-------CONSTANTS AND VARIABLES --------------------------------------------
+
 $alphabetArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+$alphabetMatrix = [['a','b','c','d','e'],['f','g','h','i','k'],['l','m','n','o','p'],['q','r','s','t','u'],['v','w','x','y','z']];
 
 $keyWord = 'kryptos';
 $keyWordArr = str_split($keyWord);
+$adjustedAlphabet1 = newAlphabet($alphabetArr, $keyWordArr);
+var_dump($adjustedAlphabet1);
 
 
+//-------FUNCTIONS --------------------------------------------
+
+
+//Creating a re-ordered alphabet array which starts with key
+// and ends in remaining alphabet characters
 function newAlphabet($alphabetArr, $keyWordArr) {
-    $diffX = array_diff($alphabetArr, $keyWordArr);
-    foreach ($diffX as $newItem) {
-        array_push($keyWordArr, $newItem);
+    //this compares array1 and array2 and returns array1 which are not present in array2
+    $nonKeyCharacters = array_diff($alphabetArr, $keyWordArr);
+
+    //F.E creats a new alphabet starting with the key array and
+    // adding on all the characters of the alphabet which
+    // are not in the key array (i.e. re-ordered alphabet)
+    foreach ($nonKeyCharacters as $addChar) {
+        array_push($keyWordArr, $addChar);
     }
     return $keyWordArr;
 };
 
-$adjustedAlphabet1 = newAlphabet($alphabetArr, $keyWordArr);
-var_dump($adjustedAlphabet1);
-
-$inputString1 = 'estd mzzv esle elfrse xp szh ez ncplep yph topyetetpd hspy t hld l acp-eppy';
-$inputString2 = 'max vhlm hy max unl wkboxk ingva b nlxw mh ingva fr hpg mktglyxkl';
-$inputString3 = 'pbzfsobp dkfobtpkx lq pbkfi ppbkfpry aoxtolc iixz lq abpr bobt pbzfsba cl bmvq obail bpbeq';
-$inputString4 = 'gsvmznvlugsnvzrmuiznvhrszxpvwzgfhxrmgsvzikzmvgwzbh';
-$inputString5 = 'jbi ujt veo eco ntk iwa lhc eeo anu uir trs hae oni rfn irt toh imi ets shs !eu';
-$inputString6 = 'dghvetpst bm ihvtl'; //test coded string with key = 'kryptos'
-$inputString7 = '';
-$inputString8 = '';
-$inputString9 = '';
 
 
 function rotateDecypher($encryptedString, $alphabetArr) {
@@ -48,6 +53,7 @@ function rotateDecypher($encryptedString, $alphabetArr) {
         return $decodedString;
 }
 
+
 function substituteDecypher($encryptedString, $alphabetArr, $alphabetArrEncrypted) {
     $decodedArr = [];
     
@@ -67,6 +73,7 @@ function substituteDecypher($encryptedString, $alphabetArr, $alphabetArrEncrypte
 }
 
 
+
 function letterToAsciAddNumberBackToLetter($character) {
     $adjustedCharacter = chr(ord($character) + 2);
     echo $adjustedCharacter . '-';
@@ -75,22 +82,26 @@ function letterToAsciAddNumberBackToLetter($character) {
 
 
 echo '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 x x x x x x' . '<br>';
+$inputString1 = 'kh lv qrw wkh phvvldkd kh lv d yhub qdxjkwb erb';
 $inputStringArr1 = str_split($inputString1);
 rotateDecypher($inputStringArr1, $alphabetArr);
 
-echo '2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 x x x x x' . '<br>';
-$inputStringArr2 = str_split($inputString2);
-rotateDecypher($inputStringArr2, $alphabetArr);
 
 echo '3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 x x x x' . '<br>';
+$inputString2 = 'pbzfsobp dkfobtpkx lq pbkfi ppbkfpry aoxtolc iixz lq abpr bobt pbzfsba cl bmvq obail bpbeq';
+$inputString3 = 'borzsm fob nxk dyb k cs csrd';
 $inputStringArr3 = str_split(strrev($inputString3));
 rotateDecypher($inputStringArr3, $alphabetArr);
 
 echo '4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 x x x x x' . '<br>';
+//test coded string with keyWord
+$inputString6 = 'dghvetpst bm ihvtl';
 $inputStringArr6 = str_split($inputString6);
 substituteDecypher($inputStringArr6, $alphabetArr, $adjustedAlphabet1);
 
 echo '5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 x x x x' . '<br>';
+
+
 
 echo '6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 x x x x x' . '<br>';
 
